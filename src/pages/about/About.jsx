bdba937f10d81ccd8c36a6b3d8a3ee621eb49e54
omotayo adegoke <omotayoadegoke@omotayos-MacBook-Pro.local>
2020-02-18 08:54:10 +0100

    home page layout done

--- src/pages/about/About.jsx
+++ src/pages/about/About.jsx
@@ -7,12 +7,21 @@ export default class About extends Component {
             <div className='page-wrapper'>
                 <div className='side-bar'>
                     
-                   <h1>Tayo Adegoke</h1>    
+                   {/*<h1>Logo</h1>  */}
+
+                   <ul>
+                      <li className="active">About</li>
+                      <li>Projects</li>
+                      <li>Résumé</li>
+                      <li>Contact</li>
+                    
+                   </ul>  
                 </div>
                 <div className='main-content'> 
                     <div className="bio-heading">
-                        {/*<img className="profile-img" src="./gitav.jpeg"></img>*/}
-                        <h1>Hello my name is Tayo Adegoke, I am a full stack developer from Lagos Nigeria. </h1>
+                        {<img className="profile-img" src="./gitav.jpeg"></img>}
+                        <h1>Hello my name is <span className="highlight">Tayo Adegoke</span>, I am a <span className={"highlight"}>full stack developer</span> from Lagos, Nigeria. </h1>
+                        <button className="btn">View My Projects</button>
                     </div>
                 </div>
 

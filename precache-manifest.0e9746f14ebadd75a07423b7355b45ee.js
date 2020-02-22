86a96e9dbdf962aa586bb377a87c9c382e2fd9ea
notthattayo <omotayoadegoke19@gmail.com>
2020-02-22 15:08:32 +0100

    Updates

---
+++ precache-manifest.0e9746f14ebadd75a07423b7355b45ee.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "01f1b90929acbdc845ade19638da0b82",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "caf0e1044038ca33bfec",
+    "url": "/portfolio/static/css/main.751d3fc1.chunk.css"
+  },
+  {
+    "revision": "0d0c01796733b5a9b0a8",
+    "url": "/portfolio/static/js/2.e61fa098.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "/portfolio/static/js/2.e61fa098.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "caf0e1044038ca33bfec",
+    "url": "/portfolio/static/js/main.126f1bba.chunk.js"
+  },
+  {
+    "revision": "0d98471f6c3b69ca4251",
+    "url": "/portfolio/static/js/runtime-main.44087e9b.js"
+  },
+  {
+    "revision": "ee7cd8ed2dcec943251eb2763684fc6f",
+    "url": "/portfolio/static/media/logo.ee7cd8ed.svg"
+  }
+]);

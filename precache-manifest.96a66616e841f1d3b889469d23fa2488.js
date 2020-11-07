e44b97f745e5c6059887e0dfd793389aebd641dd
apple <apple@apples-MacBook-Pro.local>
2020-11-07 19:28:33 +0100

    Updates

---
+++ precache-manifest.96a66616e841f1d3b889469d23fa2488.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "325d890cb68395b883396441d81c014e",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "64d0eabba8fa6bd27e5c",
+    "url": "/portfolio/static/css/main.9ed9e988.chunk.css"
+  },
+  {
+    "revision": "c0a3087d52edef89d342",
+    "url": "/portfolio/static/js/2.c8dbaab2.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "/portfolio/static/js/2.c8dbaab2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "64d0eabba8fa6bd27e5c",
+    "url": "/portfolio/static/js/main.102499d0.chunk.js"
+  },
+  {
+    "revision": "0d98471f6c3b69ca4251",
+    "url": "/portfolio/static/js/runtime-main.44087e9b.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/portfolio/precache-manifest.0f577c13424c078500a765c2dd637da7.js"
+  "/portfolio/precache-manifest.96a66616e841f1d3b889469d23fa2488.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -35,5 +35,5 @@ workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
 workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/portfolio/index.html"), {
   
-  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });

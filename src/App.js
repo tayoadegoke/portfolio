138acf7a63e37f6284551e02a5e34c8362f5d1c0
apple <apple@apples-MacBook-Pro.local>
2020-11-07 19:27:03 +0100

    redesign to single page

--- src/App.js
+++ src/App.js
@@ -1,11 +1,7 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 import About from './pages/about/About'
-import Work from './pages/work/Work'
-import Resume from './pages/resume/Resume'
-import Contact from './pages/contact/Contact'
-import { HashRouter, BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
+import { HashRouter, BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
 function App() {
   return (
@@ -13,9 +9,6 @@ function App() {
     <div className="App">
      <Switch>
           <Route exact path='/' component={About}></Route>
-          <Route exact path='/projects' component={Work}></Route>
-          <Route exact path='/resume' component={Resume}></Route>
-          <Route exact path='/contact' component={Contact}></Route>
      </Switch>
      
     </div>
--- src/pages/about/About.jsx
+++ src/pages/about/About.jsx
@@ -1,51 +1,72 @@
 import React, { Component } from 'react'
-import {Link} from 'react-router-dom'
 import './about.css'
-import Nav from '../../components/Nav/Nav'
+
+
 export default class About extends Component {
     
     state = {
         nav:null
     }
 
-   navStyles={
-        display:"block",
-        left: "0",
-        transition: "all 0.5s",
-        width: "100%"
-       
+     goTo = (id) => {
+        var element = document.getElementById(id);
+        element.scrollIntoView({behavior: "smooth", block: "end", inline: "nearest"});
     }
 
-    navStylesclose = {
-        left:"-600px",
-        transition:"all 0.5s"
-    }
-
-     toggleNav = ( )=>{
-         if(this.state.nav==null || this.state.nav==this.navStylesclose){
-        this.setState({nav:this.navStyles})
-         }else if(this.state.nav == this.navStyles){
-             this.setState({nav:this.navStylesclose})
-         }
-    }
     render() {
-
-        
-
-      
-       
         return (
             <div className='page-wrapper'>
-               <Nav navStyles={this.state.nav} toggleNav={this.toggleNav}active={"about"}></Nav>
-                <div className='main-content'> 
-                <img  src="./hamb.png" onClick={this.toggleNav}className={"hamb"}></img>
+                <div className='navbar'>
+                   <ul>
+                       <li onClick={()=>this.goTo('home')}>Home</li>
+                       <li onClick={()=>this.goTo('work')}>Projects</li>
+                       <li onClick={()=>this.goTo('contact')}>Contact</li>
+                   </ul>
+                </div>
+                <div className='main-content' id='home'> 
+                {/* <img  src="./hamb.png" onClick={this.toggleNav}className={"hamb"}></img> */}
                     <div className="bio-heading">
-                     
-                        <h1>Hello my name is <span className="highlight">Tayo Adegoke</span>, I am a <span className={"highlight"}>full stack web developer</span> from Lagos, Nigeria. </h1>
-                        <Link to="/projects"><button className="btn">View My Projects</button></Link>
+                        <img className="profile-img" src="./gitav.jpeg"></img>
+                        <h1>Hello, welcome to my portfolio. <br/> My name is <span className="highlight">Tayo Adegoke</span> and I am a <span className={"highlight"}>full stack web developer</span> from Lagos, Nigeria. </h1>
+                        <button onClick={()=>this.goTo('work')} className="btn">View My Projects</button>
                     </div>
                 </div>
+                <div id='work'>
+                <div  className='main-content'>
+                <div className="bio-heading" style={{paddingTop:'4em'}}>
+                  <h1 className='title'>Projects</h1>
+                      <p>Below you can find links to some of my recent personal projects</p>
+                   <a target = "_blank" rel="noopener noreferrer" href = "https://social-app-7a613.web.app/"> 
+                       <div className='card'>
+                       <h3>Twitter Clone</h3>
+                       <img src="./twit.png"></img>
+                    </div>
+                  </a>
+                </div>
+                </div>
+                </div>
 
+                <div id='contact'>
+                <div  className='main-content'>
+                <div className="bio-heading" style={{paddingTop:'4em'}}>
+                <h1 className='title'>Contact</h1>
+                
+                <h3>Got something Interesting? <span className="highlight">Let's Chat</span></h3>
+                      
+                      <div className='container-div'>
+                          <div>
+                              <a href= "mailto: omotayoadegoke19@gmail.com" rel="noopener noreferrer"><img  className="contactimg"src="./mail.png"></img></a>
+                          </div>
+                          <div>
+                             <a target = "_blank" href = "https://ng.linkedin.com/in/omotayo-adegoke-33a3a3120" rel="noopener noreferrer"><img  className="contactimg"src="./linkedin.png"></img></a> 
+                          </div>
+                          <div>
+                             <a target = "_blank" href = "https://github.com/notthattayo" rel="noopener noreferrer">  <img  className="contactimg"src="./github.png"></img></a>
+                          </div>
+                      </div>
+                </div>
+                </div>
+                </div>
                 
             </div>
         )

dd24017119235e330bd380716b9524684e521981
apple <omotayoadegoke19@gmail.com>
2021-04-03 09:43:48 +0100

    Updates

---
+++ precache-manifest.a766ef447f33fcf448704323ebe06bbe.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "251c18b96ff98710ff27a5852d6d18b1",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "9b8f6a07e21fc34e7906",
+    "url": "/portfolio/static/css/main.044aa1ec.chunk.css"
+  },
+  {
+    "revision": "c0a3087d52edef89d342",
+    "url": "/portfolio/static/js/2.c8dbaab2.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "/portfolio/static/js/2.c8dbaab2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9b8f6a07e21fc34e7906",
+    "url": "/portfolio/static/js/main.cc67ea03.chunk.js"
+  },
+  {
+    "revision": "0d98471f6c3b69ca4251",
+    "url": "/portfolio/static/js/runtime-main.44087e9b.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/portfolio/precache-manifest.07e595c152aa52fcde0b863e5791da1d.js"
+  "/portfolio/precache-manifest.a766ef447f33fcf448704323ebe06bbe.js"
 );
 
 self.addEventListener('message', (event) => {

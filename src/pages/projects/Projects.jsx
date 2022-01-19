1eff4ed39fe2dc6f313130683390b614840c6cc8
apple <omotayoadegoke19@gmail.com>
2022-01-19 20:21:17 +0000

    fixed issues on projects page

--- src/pages/projects/Projects.jsx
+++ src/pages/projects/Projects.jsx
@@ -33,53 +33,55 @@ function Projects() {
             onMouseEnter={() => flipCard("enter", "netflix-review")}
             onMouseLeave={() => flipCard("leave", "netflix-review")}
           >
-            <a
-              target="_blank"
-              rel="noopener noreferrer"
-              href="https://netflix-review-a96cf.web.app/"
-            >
-              <div className="card">
-                <h3 id="netflix-review-head">Netflix Review</h3>
-                <img src="./netflix.png" id="netflix-review-img"></img>
-                <div className="card-details" id="netflix-review-details">
-                  <p>
-                    <span>About : </span> An application to review netflix
-                    movies and watch movie trailers
-                  </p>
-                  <p>
-                    <span>Tech Stack: </span> React , Material UI, NodeJs ,
-                    Express, MongoDb and Firebase{" "}
-                  </p>
-                  <button>View Application</button>
-                </div>
+            <div className="card">
+              <h3 id="netflix-review-head">Netflix Review</h3>
+              <img src="./netflix.png" id="netflix-review-img"></img>
+              <div className="card-details" id="netflix-review-details">
+                <p>
+                  <span>About : </span> An application to review netflix movies
+                  and watch movie trailers
+                </p>
+                <p>
+                  <span>Tech Stack: </span> React , Material UI, NodeJs ,
+                  Express, MongoDb and Firebase{" "}
+                </p>
+                <a
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  href="https://netflix-review-a96cf.web.app/"
+                >
+                  {" "}
+                  <button>View Application</button>{" "}
+                </a>
               </div>
-            </a>
+            </div>
           </div>
           <div
             className="project-card-heading"
             onMouseEnter={() => flipCard("enter", "twitter-clone")}
             onMouseLeave={() => flipCard("leave", "twitter-clone")}
           >
-            <a
-              target="_blank"
-              rel="noopener noreferrer"
-              href="https://social-app-7a613.web.app/"
-            >
-              <div className="card">
-                <h3 id="twitter-clone-head">Twitter Clone</h3>
-                <img src="./twit.png" id="twitter-clone-img"></img>
-                <div className="card-details" id="twitter-clone-details">
-                  <p>
-                    <span>About : </span> A Simple Twitter clone with Minimal
-                    Functionality.
-                  </p>
-                  <p>
-                    <span>Tech Stack: </span> React , Material UI, and Firebase
-                  </p>
+            <div className="card">
+              <h3 id="twitter-clone-head">Twitter Clone</h3>
+              <img src="./twit.png" id="twitter-clone-img"></img>
+              <div className="card-details" id="twitter-clone-details">
+                <p>
+                  <span>About : </span> A Simple Twitter clone with Minimal
+                  Functionality.
+                </p>
+                <p>
+                  <span>Tech Stack: </span> React , Material UI, and Firebase
+                </p>
+                <a
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  href="https://social-app-7a613.web.app/"
+                >
+                  {" "}
                   <button>View Application</button>
-                </div>
+                </a>
               </div>
-            </a>
+            </div>
           </div>
         </div>
       </div>

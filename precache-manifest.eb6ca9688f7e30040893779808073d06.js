8e673d56dea5458fde8200bd6febe0b635a7f708
apple <omotayoadegoke19@gmail.com>
2023-01-25 13:55:25 +0000

    Updates

--- precache-manifest.eb6ca9688f7e30040893779808073d06.js
+++ precache-manifest.eb6ca9688f7e30040893779808073d06.js
@@ -1,6 +1,6 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "553823728136970588c2524646e0d711",
+    "revision": "5e36ec7eae994324f3fee4962d931411",
     "url": "/portfolio/index.html"
   },
   {
@@ -8,8 +8,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/portfolio/static/css/2.7abef457.chunk.css"
   },
   {
-    "revision": "1d9e6f35e149108a924b",
-    "url": "/portfolio/static/css/main.6f11aa0d.chunk.css"
+    "revision": "a32b74dd6aa1be2cbe62",
+    "url": "/portfolio/static/css/main.d1a46c9b.chunk.css"
   },
   {
     "revision": "8619400d7e4d8cbc4535",
@@ -20,7 +20,7 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/portfolio/static/js/2.a38d00c9.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "1d9e6f35e149108a924b",
+    "revision": "a32b74dd6aa1be2cbe62",
     "url": "/portfolio/static/js/main.0f1629a1.chunk.js"
   },
   {
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/portfolio/precache-manifest.64904e00cf4b33f521ed89f1d741085f.js"
+  "/portfolio/precache-manifest.eb6ca9688f7e30040893779808073d06.js"
 );
 
 self.addEventListener('message', (event) => {
